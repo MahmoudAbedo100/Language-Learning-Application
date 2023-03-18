fe228b807dac8442a05c8025a55e33acdc8215e8
MahmoudAbedo100 <125769619+MahmoudAbedo100@users.noreply.github.com>
2023-03-18 14:43:49 +0000

    Delete Add_flash_cards.py

--- Add_flash_cards.py
+++
@@ -1,7 +0,0 @@
-from googletrans import Translator
-
-translator = Translator()
-
-word_en = input("Type a word in english to get the french translation: ")
-word_fr = translator.translate(word_en, src="en", dest="fr").text
-print(word_fr)
